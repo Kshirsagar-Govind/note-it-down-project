37aef4fda499f62a084635b3f7c758a3373c58e3
Kshirsagar-Govind <88773342+Kshirsagar-Govind@users.noreply.github.com>
2022-02-24 22:22:14 +0530

    new changes

--- src/Components/Pages/tasks.jsx
+++ src/Components/Pages/tasks.jsx
@@ -1,5 +1,6 @@
 import React, { Component, useState } from "react";
 import Colors from "../Helpers/colors";
+import CloseButton from "../Helpers/close-button";
 
 import Logo from "../Assets/SVG-JSX/home-logo";
 import ArrowDown from "../Assets/SVG/arrow-down.svg";
@@ -12,8 +13,20 @@ class TasksPage extends Component {
     super(props);
     this.state = {
       selectedColor: "",
+      addMore: [],
+      showAddTasks: false,
     };
   }
+
+  AddMoreInput = () => {
+    // alert("ok");
+    let arr = [ ...this.state.addMore ];
+    arr.push(1);
+
+    this.setState({
+      addMore: arr,
+    });
+  };
 
   render() {
     return (
@@ -25,9 +38,75 @@ class TasksPage extends Component {
             {Colors.map(item => <TasksCard color={item.color} />)}
           </div>
         </div>
+        {this.state.showAddTasks ? (
+          <div className="dark-back just-center">
+            <div className="add-note-popup">
+              <div className="note-popup-header just-space">
+                <h1>Add New Task +</h1>
+                <CloseButton
+                  callback={() => {
+                    this.setState({ showAddTasks: false });
+                  }}
+                />
+              </div>
+
+              <div className="note-popup-form ">
+                <div className=" m-yy-20">
+                  <label className="head-16-semi col-100" htmlFor="">
+                    Tasks Title
+                  </label>
+                  <span className="input-wrapper">
+                    <input className="input-box head-16-semi" type="text" />
+                  </span>
+                </div>
+
+                <div className=" m-yy-20">
+                  <label className="head-16-semi col-100" htmlFor="">
+                    Task
+                  </label>
+                  <span className="input-wrapper">
+                    <input className="input-box head-16-semi" type="text" />
+                  </span>
+                  <span className="button-wrapper">
+                    <button
+                      onClick={() => this.AddMoreInput()}
+                      className="secondary_button head-16-semi m-xx-20"
+                    >
+                      {" "}
+                      +{" "}
+                    </button>
+                  </span>
+                </div>
+                {this.state.addMore.map(item => (
+                  <div className=" m-yy-20">
+                    <label className="head-16-semi col-100" htmlFor="">
+                      Task
+                    </label>
+                    <span className="input-wrapper">
+                      <input className="input-box head-16-semi" type="text" />
+                    </span>
+                  </div>
+                ))}
+                <div className="just-space">
+                  <div />
+                  <span className="button-wrapper">
+                    <button className="primary_button head-16-semi">
+                      Add Tasks
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null}
 
         <div className="floating-button">
-          <FloatingButton text="Add New Tasks +" callback={() => {}} />
+          <FloatingButton
+            text="Add New Tasks +"
+            callback={() => {
+              this.setState({ showAddTasks: true });
+            }}
+          />
         </div>
       </div>
     );
